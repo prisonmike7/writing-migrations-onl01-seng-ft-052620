a775967b89f233bb542b51fb24290c9ac1f6de0d
Michael Harris <mwharris7@gmail.com>
2020-07-02 12:05:04 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,13 +1,6 @@
 
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
-
   def change
-    add_column :students do |t|
-      t.integer :grade
-      t.datetime :birthdate
-    end
+    change_column :students, :birthdate, :datetime
   end
-
-
-
 end
